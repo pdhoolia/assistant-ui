b000cb6a9146f994a08147a21b3e56aa855eeed8
Pankaj Dhoolia <pdhoolia@in.ibm.com>
2025-03-24 18:24:05 +0530

    when title generation fails let's use a slightly smarter default

--- packages/react/src/runtimes/remote-thread-list/RemoteThreadListThreadListRuntimeCore.tsx
+++ packages/react/src/runtimes/remote-thread-list/RemoteThreadListThreadListRuntimeCore.tsx
@@ -423,7 +423,7 @@ export class RemoteThreadListThreadListRuntimeCore
     for await (const result of messageStream) {
       const newTitle =
         result.content.filter((c) => c.type === "text")[0]?.text ??
-        "New Thread";
+        (messages[0]?.content.filter(c => c.type === "text")[0]?.text?.substring(0, 15) + '...' || 'New Thread');
       const state = this._state.baseValue;
       this._state.update({
         ...state,
